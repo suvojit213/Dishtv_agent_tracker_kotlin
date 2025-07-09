16bffe196aafa30df39c579a7004b703f7af90cc
suvojit213 <suvojitsengupta21@gmail.com>
2025-07-09 11:13:29 +0530

    Fix: Use fully qualified names for KeyboardOptions and KeyboardType

--- app/src/main/java/com/suvojeet/issuetracker/screens/SettingsScreen.kt
+++ app/src/main/java/com/suvojeet/issuetracker/screens/SettingsScreen.kt
@@ -52,8 +52,8 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.input.KeyboardOptions
-import androidx.compose.ui.text.input.KeyboardType
+
+
 import androidx.compose.foundation.layout.Box
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
@@ -177,7 +177,7 @@ fun SettingsScreen(navController: NavController) {
                     label = { Text("Enter password") },
                     singleLine = true,
                     visualTransformation = androidx.compose.ui.text.input.PasswordVisualTransformation(),
-                    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password)
+                    keyboardOptions = androidx.compose.ui.text.input.KeyboardOptions(keyboardType = androidx.compose.ui.text.input.KeyboardType.Password)
                 )
             },
             confirmButton = {
